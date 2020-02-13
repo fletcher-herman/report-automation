1f958b1ff98e3835f8bd8af537180a03697b4405
Fletcher Herman <fletcher.herman@COTTON-ON.LOCAL>
2020-02-13 11:49:34 +1100

    update join parameters

--- ReportAutomationFunctions.py
+++ ReportAutomationFunctions.py
@@ -130,12 +130,8 @@ def agePro_func(sql_conn):
     tran_summ = sql_conn(age_pro_path, server, db)
     print('transaction file exported')
     
-    # drop irrelevant rows
-    drop = ['Missing']    
-    perks_c = perks[~perks['age_band'].isin(drop)]
-    
     # build report
-    ageProfile = pd.merge(perks_c[['customer_id','age_segment']],
+    ageProfile = pd.merge(perks[['customer_id','age_segment']],
                  tran_summ,
                  how='inner',
                  on='customer_id')
